ce725a4cd609a1c28950d72de1d82231eeaa3860
jason80817 <jason80817@gmail.com>
2025-01-26 08:13:16 +0000

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:myapp/screens/start_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: StartScreen(),
+      theme: ThemeData(useMaterial3: true),
+    );
+  }
+}
---
+++ lib/screens/start_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:myapp/screens/game_screen.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text("타이밍 게임", style: TextStyle(fontSize: 40),),
+            Container(height: 70,),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                padding: EdgeInsets.symmetric(
+                  vertical: 10
+                  ,horizontal: 20
+                )
+              ),
+              onPressed: () {
+                Navigator.of(
+                  context,
+                ).push(MaterialPageRoute(builder: (context) => GameScreen()));
+              },
+              child: Text("게임 시작", style: TextStyle(fontSize: 20),),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
